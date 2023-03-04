2b2af10f7f0a007709d1bf61afef313e3e84e64c
Lucas Källberg <lucas.arne@hotmail.com>
2023-03-04 20:36:27 +0100

    started on register

---
+++ quizmaster/resources/views/register.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/app.css">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <form class="flex-container">
+            <h1>Register</h1>
+            <label>Name:</label>
+            <input type="text" >
+            <button>Login</button>
+            <button>Reqister</button>
+        </form>
+    </main>
+</body>
+</html>
